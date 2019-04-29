0562b8d862d911c09329b20b68bb38e6b208af1d
Luke Mirman <OrangeJuicePanda1@gmail.com>
2019-04-28 21:41:26 -0400

    Fixed a minor bug with modify menu

--- Assets/Scripts/UI/MenuModifyAutofill.cs
+++ Assets/Scripts/UI/MenuModifyAutofill.cs
@@ -31,14 +31,14 @@ public class MenuModifyAutofill : MonoBehaviour
 			case AutofillTarget.Repair:
 				ProductionBuilding building = choice.targetTile.GetComponent<ProductionBuilding>();
 				useSign = building.RepairCost != 0;
-				cost.SetText(useSign ? "-" : "" + building.RepairCost.ToString());
+				cost.SetText(useSign ? "-" + building.RepairCost.ToString() : "" + building.RepairCost.ToString());
 				title.SetText(title.text + " (" + building.durability + "/" + building.thisBuilding.durability + ")");
 				cost.color = useSign ? new Color(0.25f, 0f, 0f) : Color.black;
 				break;
 			//For the sell type we set the cost field to the amount refuned to the player.
 			case AutofillTarget.Sell:
 				useSign = choice.targetTile.building.value != 0;
-				cost.SetText(useSign ? "+" : "" + choice.targetTile.building.value);
+				cost.SetText(useSign ? "+" + choice.targetTile.building.value : "" + choice.targetTile.building.value);
 				cost.color = useSign ? new Color(0f, 0.25f, 0f) : Color.black;
 				break;
 		}
